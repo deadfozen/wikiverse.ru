fbde62a4b7413688ff5297d7ccd63c7e961416a0
deadfozen <deadfozen430@gmail.com>
2026-03-08 19:50:50 +0500

    page.tsx

---
+++ src/app/about/page.tsx
@@ -0,0 +1,18 @@
+import {Globe} from "lucide-react";
+import {Card, CardContent, CardHeader} from "@/components/ui/card";
+import {Separator} from "@/components/ui/separator";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
+export const metadata = {
+    title: "О проекте — WikiVerse",
+};
+export default function AboutPage() {
+    return (
+        <div className="flex flex-col min-h-screen">
+            <Header/>
+            <div className="min-h-screen bg-gradient-to-b from-background ">
+            
+            </div>
+        </div>
+    )
+}
